336c1e65e0d49adacc04e1634ad708e9736f8f1a
zldeng <hitdzl@gmail.com>
2017-09-07 10:40:35 +0800

    add main.cpp file to repositoy

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (C) 2017 All rights reserved.
+* file : main.cpp
+* author : zldeng@mioji.com
+* date : 2017-09-07
+* description : 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char** argv)
+{
+	cout << "hello world!" << endl;
+
+	return 0;
+}
